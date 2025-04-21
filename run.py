6a96ba4ab1afe3e2663ff0486598b18298e8168c
Asela <asela@paragonsoft.net>
2025-04-22 00:20:05 +0530

    Remove unused files and clean up application structure

--- run.py
+++ run.py
@@ -17,15 +17,21 @@
 #     app.run()
 
 
-import os
+# import os
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     # This is only for local testing
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
 from app import create_app
 
-app = create_app()
-
 if __name__ == "__main__":
-    # This is only for local testing
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
+    app = create_app()
+    app.run(debug=True,port=8080)
 
 # import os
 # from flask import Flask
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn -w 4 -b 0.0.0.0:3000 app:app
